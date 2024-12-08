6a9ffa73ab901a63f6e3578a6ddb383c3082c6d2
minjae k <rkdalswoals@gmail.com>
2024-12-08 21:40:01 +0900

    feat(WinningNumberGenerator): 정답 랜덤 숫자 생성 기능 테스트 및 구현

--- src/main/java/baseball/WinningNumberGenerator.java
+++ src/main/java/baseball/WinningNumberGenerator.java
@@ -3,11 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class Computer {
+public class WinningNumberGenerator {
 
     public List<Integer> createWinningNumber() {
         return Randoms.pickUniqueNumbersInRange(1,9,3);
     }
-
-
 }
